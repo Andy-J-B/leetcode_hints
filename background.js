56dcb59b73f539a1a1916610f44d14bd8344c274
Andy-J-B <080.abae@gmail.com>
2025-05-20 22:13:47 -0700

    Added a listener to listen into tab changes

--- background.js
+++ background.js
@@ -1,12 +1,19 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // Only proceed if the page has finished loading and has a valid URL
+  if (
+    changeInfo.status === "complete" &&
+    tab.url &&
+    tab.url.includes("leetcode.com/problems")
+  ) {
+    console.log("LeetCode problem detected:", tab.url);
+
+    // Extract problem name from the URL path, e.g., https://leetcode.com/problems/two-sum/
+    const pathSegments = new URL(tab.url).pathname.split("/");
+    const problemId = pathSegments[2]; // "two-sum"
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      problemId: problemId,
+    });
+  }
+});
